8ba2bc7cee80e39f81bc5e44aee13909ae553e75
Temirlan <153745312+temirlannz@users.noreply.github.com>
2024-02-10 03:10:16 +0600

    nav abs

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -20,7 +20,7 @@ const Navbar = () => {
     }, []);
 
     return (
-        <nav className='mx-auto max-w-7xl h-16 flex justify-between items-center px-4 sm:px-6 lg:px-8'>
+        <nav className='absolute top-0 left-0 mx-auto max-w-7xl h-16 flex justify-between items-center px-4 sm:px-6 lg:px-8'>
             <Link href='/sign-in' className='text-sm sm:text-md font-medium'>
                 Antipoff
             </Link>
